17032bda570a8661ad0d30a5a330d265ee46b660
James Middleton <james@midz.dev>
2024-10-14 13:44:46 +0100

    override timestamp in development

--- atoms/timestamp.ts
+++ atoms/timestamp.ts
@@ -6,8 +6,13 @@ import {
   unwrap,
   unstable_withStorageValidator as withStorageValidator,
 } from "jotai/utils";
-import { useEffect, useRef } from "react";
+import { useEffect, useMemo, useRef } from "react";
 import { AppState } from "react-native";
+
+const OVERRIDE_TIME_AGO = null;
+// process.env.NODE_ENV === "development"
+//   ? 24 * 60 * 60 * 1000 - 10 * 1000
+//   : null;
 
 const asyncStorage = createJSONStorage(() => AsyncStorage);
 
@@ -24,17 +29,30 @@ const storedTimestampAtom = atomWithStorage(
 
 const resetTimestampAtom = atom(null, async (_, set) => {
   console.log("resetting timestamp");
-  await set(storedTimestampAtom, Date.now());
+
+  const newValue = OVERRIDE_TIME_AGO
+    ? Date.now() - OVERRIDE_TIME_AGO
+    : Date.now();
+
+  await set(storedTimestampAtom, newValue);
 });
 
 const unwrappedStoredTimestampAtom = unwrap(storedTimestampAtom);
 
 export const useTimestamp = () => {
-  const [value] = useAtom(unwrappedStoredTimestampAtom);
+  const [storedValue] = useAtom(unwrappedStoredTimestampAtom);
 
   useEffect(() => {
-    console.log("timestamp changed", value);
-  }, [value]);
+    console.log("stored timestamp changed", storedValue);
+  }, [storedValue]);
+
+  const value = useMemo(() => {
+    if (OVERRIDE_TIME_AGO) {
+      return Date.now() - OVERRIDE_TIME_AGO;
+    }
+
+    return storedValue;
+  }, [storedValue]);
 
   return value;
 };
@@ -49,27 +67,9 @@ export const useResetTimestampOnBlur = () => {
         return;
       }
 
-      const time = new Date().toTimeString();
-      console.log(
-        time,
-        `App state changed from ${prevState.current} to ${state}`
-      );
-
-      const message = `App state changed from ${prevState.current} to ${state}`;
-      setTimeout(() => {
-        console.log(time, message);
-      }, 2000);
-
-      if (
-        prevState.current.match(/inactive|background/) &&
-        state === "active"
-      ) {
-        console.log("App has come to the foreground!");
-      }
+      console.log(`App state changed from ${prevState.current} to ${state}`);
 
       if (prevState.current.match(/active/) && state === "background") {
-        console.log("App is going to background");
-
         try {
           await resetTimestamp();
 
