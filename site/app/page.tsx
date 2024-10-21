b29991bba2e0cee901a00b15c8a03895b8daafd2
James Middleton <james@midz.dev>
2024-10-21 23:28:24 +0100

    init nextjs site

---
+++ site/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex flex-row justify-center items-center">
+      <p className="font-semibold text-5xl">Under construction</p>
+    </main>
+  );
+}
